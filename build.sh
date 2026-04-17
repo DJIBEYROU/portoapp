3e92e48ca87c794d12713616ca12837679454351
Djibeyrou Ba <djibeyrou@example.com>
2026-04-17 15:40:40 +0900

    Fix render.yaml

--- build.sh
+++ build.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 # Build script for Render
-
-# Install dependencies
 pip install -r requirements.txt
-
-# Apply migrations
 python manage.py migrate --noinput
-
-# Collect static files
 python manage.py collectstatic --noinput
